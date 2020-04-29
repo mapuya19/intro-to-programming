419ba94aebbb2efeda38d37e85b5a5448997ae12
Matthew Apuya <m.apuya19@gmail.com>
2020-04-28 22:44:33 -0500

    edited comments

--- Homework7/upc.py
+++ Homework7/upc.py
@@ -1,44 +1,11 @@
-# Matthew Apuya
-# 11/10/19
-# CSCI-UA.00003-001
-
 """
-upc.py
-=====
-Implement the following two functions as specified in the docstrings:
-
-1. generate_bar_widths(s)
-2. valid_barcode(s)
-
-Some resources that may help with your implementation:
-
-* https://en.wikipedia.org/wiki/Universal_Product_Code#Encoding
-* http://electronics.howstuffworks.com/gadgets/high-tech-gadgets/upc3.htm
-* http://www.adams1.com/upccode.html
-
+Takes a barcode number as a string and translates it to a series
+    of bar widths (a string that consists of 1's, 2's, 3's and 4's with
+    each number corresponding to a width of a bar).
 """
 
 
 def generate_bar_widths(s):
-    """Takes a barcode number as a string and translates it to a series
-    of bar widths (a string that consists of 1's, 2's, 3's and 4's with
-    each number corresponding to a width of a bar). For example, a
-    series of bar widths starting with '1113 ... ' would consists of
-    a black single width bar, a white single width bar, a black single
-    width bar... and then a white triple width bar'.
-
-    generate_bar_widths('043000181706')
-    # --> '11132111132141132113211321111111222112132221131232111114111'
-
-    HINT: try to do this by using the index and elements of a list to
-    map between a number and a series of bar_widths
-
-    :param s: the number to be translated to a series of bar widths
-    :type s: str
-    :return: a string representing the width of each bar, including the
-    start, middle and end patterns (111, 11111, and 111)
-    :rtype: str
-    """
     bar_width = "111"
     counter = 0
 
@@ -75,20 +42,6 @@ def generate_bar_widths(s):
 
 
 def valid_barcode(s):
-    """Determines whether a barcode is valid or not based on length and
-    the check digit. A "UPC-A" barcode consists of 12 digits, with the
-    last digit being the check digit. Some examples:
-
-    valid_barcode('036000291452') # --> True
-    valid_barcode('036000291450') # --> False
-    valid_barcode('075678164125') # --> True
-    valid_barcode('')            # --> False
-
-    :param s: barcode number
-    :type s: str
-    :return: true if the barcode is valid, false otherwise
-    :rtype: bool
-    """
     is_valid = False
     has_check = False
     odd_barcode_list = s[0::2]
